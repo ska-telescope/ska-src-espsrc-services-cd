155c84a81c651ba971eb1bf59f2bf9624056c606
Manuel Jesús Parra Royón <manuparra@gmail.com>
2024-11-07 16:13:23 +0000

    documentation ready

---
+++ docs/source/config.py
@@ -0,0 +1,26 @@
+# docs/source/conf.py
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'My Project'
+author = 'Tu Nombre'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx-book-theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. My Project documentation master file, created by
+   sphinx-quickstart on Thu Jan 01 2024.
+
+Welcome to My Project's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   introduction
+   installation
+   some_module
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
